051671990c534a5f24ff0b449b0d2f12229641d9
ddthj <mwirsing@rthighschool.org>
2016-01-28 15:44:33 -0500

    Todo List and first car ai
    les go

--- src/Simulator/Sim.java
+++ src/Simulator/Sim.java
@@ -1,5 +1,15 @@
 package Simulator;
 
 public class Sim {
-
+	public static void Main(String Args){
+		//Things we need to thing:
+		//
+		// - Make the game window
+		// - render out a road/a test car (just a square)
+		// - set up camera movements/locking
+		// - set up primary sim variables (top speed, exit speed, cars joining per second, time, sim hz, camera start x and y positions, road dimensions and positions....)
+		// - set up sim, create cars at random points
+		// - set up sim "physics," handle movements of cars (or attempts at movements) crashes (cars intersecting) etc
+		// - set up car AI's
+	}
 }
