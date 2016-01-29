edaabe6da03bf19d390b1b718967a71cca0f9b4f
ddthj <mwirsing@rthighschool.org>
2016-01-29 14:24:58 -0500

    AHH
    BATTERY LOW GOTTA SAVE

--- src/Simulator/Sim.java
+++ src/Simulator/Sim.java
@@ -1,17 +1,14 @@
 package Simulator;
 import java.awt.Dimension;
 import java.awt.Graphics;
-
+import java.awt.Graphics2D;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-
 import java.awt.Color;
 import java.awt.image.BufferStrategy;
 
 public class Sim{
-	public static void main(String[] Args){
-		boolean running = true;
-		
+	public static void main(String[] Args){		
 		//Things we need to thing:
 		//
 		//50% - Make the game window
@@ -28,14 +25,29 @@ public class Sim{
 	}
 	
 	public Sim(){
+		int camX = 0;
+		int camY = 0;
 		JFrame frame = new JFrame("TrafficSim");
 		JPanel panel = (JPanel) frame.getContentPane();
-		panel.setPreferredSize(new Dimension(500,500));
+		panel.setPreferredSize(new Dimension(600,900));
 		panel.setBounds(0,0,800,600);
 		frame.setIgnoreRepaint(true);
 		frame.pack();
 		frame.setVisible(true);
 		frame.createBufferStrategy(2);
 		BufferStrategy strategy = frame.getBufferStrategy();
+		boolean running = true;
+		while(running){
+			Graphics2D g = (Graphics2D) strategy.getDrawGraphics();
+			g.setColor(Color.green);
+			g.fillRect(0, 0, 600, 1000);
+			g.setColor(Color.black);
+			g.fillRect(200, 0, 200-camX, 1000-camY);
+			//this is where the render function will be called... IF WE CODED ONE!
+			//for car in arraylist, car.render(g);
+			g.dispose();
+			strategy.show();
+			try { Thread.sleep(10); } catch (Exception e) {}		
+		}
 	}
 }
